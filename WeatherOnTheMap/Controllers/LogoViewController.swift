09d8dafdb67a4361515454949e61f1c89fd37e6a
Marina Huber <huber.marina@gmail.com>
2018-05-05 01:00:25 +0200

    Add Lottie animations on custom LaunchScreen

--- WeatherOnTheMap/Controllers/LogoViewController.swift
+++ WeatherOnTheMap/Controllers/LogoViewController.swift
@@ -10,20 +10,26 @@ import UIKit
 import Lottie
 
 class LogoViewController: UIViewController {
-
-	let animateView = LOTAnimationView(name: "loader_animation")
+	let animateView = LOTAnimationView(name: "logo_final")
+	let animateView2 = LOTAnimationView(name: "loader_animation")
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		animateView.frame = CGRect(x: 0, y: 0, width: view.frame.size.width, height: view.frame.size.height)
-		animateView.contentMode = .scaleAspectFill
+		self.navigationController?.navigationBar.setBackgroundImage(UIImage(), for: UIBarMetrics.default)
+		self.navigationController?.navigationBar.shadowImage = UIImage()
+		self.navigationController?.navigationBar.isTranslucent = true
+		self.navigationController?.view.backgroundColor = UIColor.clear
+		animateView.frame = CGRect(x: 0, y: 0, width: view.frame.size.width, height: view.frame.size.height/2)
+		animateView2.frame = CGRect(x: 0, y: view.frame.size.height/2, width: view.frame.size.width, height: view.frame.size.height/2)
+		animateView.contentMode = .scaleAspectFit
+		animateView2.contentMode = .scaleAspectFit
 		view.addSubview(animateView)
+		view.addSubview(animateView2)
 		animateView.play()
+		animateView2.play()
 
-		UIView.animate(withDuration: 0.1, delay: 4.0, options: .curveEaseIn, animations: {
+		UIView.animate(withDuration: 1, delay: 3.0, options: .curveEaseIn, animations: {
 			self.view.alpha = 0
-
-
 
 		}, completion: { finished in
 			self.switchScreen()
