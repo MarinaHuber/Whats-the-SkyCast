59ebe77fa1ef3fb161ae5c1607530f093a6f9621
Marina Huber <huber.marina@gmail.com>
2018-07-26 17:02:38 +0200

    added About and private policy

--- WeatherOnTheMap/Controllers/AboutViewController.swift
+++ WeatherOnTheMap/Controllers/AboutViewController.swift
@@ -2,51 +2,44 @@
 //  AboutViewController.swift
 //  WeatherOnTheMap
 //
-//  Created by Marina Huber on 9/26/17.
-//  Copyright © 2017 Marina Huber. All rights reserved.
+//  Created by Marina Huber on 7/26/18.
+//  Copyright © 2018 Marina Huber. All rights reserved.
 //
 
+import Foundation
 import UIKit
-import WebKit
 
-class AboutViewController: UIViewController, WKNavigationDelegate, WKUIDelegate {
-	var webView: WKWebView!
-	
-	@IBOutlet weak var activity: UIActivityIndicatorView!
-	
-	
+
+class AboutViewController: UIViewController {
+
+
+	@IBOutlet weak var websiteButton: UIButton!
+	@IBOutlet weak var creditsButton: UIButton!
+	@IBOutlet weak var privacyButton: UIButton!
+
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		loadHtmlFile()
-		activity.alpha = 1
-		activity.startAnimating()
-		
+
+		websiteButton.layer.borderWidth = 1.5
+		websiteButton.layer.borderColor = UIColor.black.cgColor
+		websiteButton.layer.cornerRadius = 5
+
+
+		privacyButton.layer.borderWidth = 1.5
+		privacyButton.layer.borderColor = UIColor.black.cgColor
+		privacyButton.layer.cornerRadius = 5
+
+		creditsButton.layer.borderWidth = 1.5
+		creditsButton.layer.borderColor = UIColor.black.cgColor
+		creditsButton.layer.cornerRadius = 5
 	}
-	
-	override func viewDidAppear(_ animated: Bool) {
-		super.viewDidAppear(true)
-		UIView.animate(withDuration: 2.1, animations: {
-			self.activity.alpha = 0
-			
-		}, completion: nil)
+
+	@IBAction func websiteCall(_ sender: Any) {
+		if let url = URL(string: "http://www.marinahuber.com"),
+			UIApplication.shared.canOpenURL(url) {
+			UIApplication.shared.open(url, options: [:])
+		}
 	}
-	
-	
-	func loadHtmlFile() {
-		webView = WKWebView(frame: CGRect( x: 0, y: 60, width: view.frame.width, height: view.frame.height - 20 ), configuration: WKWebViewConfiguration() )
-		webView.navigationDelegate = self
-		view.insertSubview(webView, belowSubview: activity)
-		webView.allowsBackForwardNavigationGestures = true
-		//        webView.translatesAutoresizingMaskIntoConstraints = false
-		//        webView.addConstraint(NSLayoutConstraint(item: webView, attribute: .left, relatedBy: .equal, toItem: webView, attribute: .right, multiplier: 1.0, constant: 0.0))
-		//        webView.addConstraint(NSLayoutConstraint(item: webView, attribute: .right, relatedBy: .equal, toItem: webView, attribute: .right, multiplier: 1.0, constant: 0.0))
-		guard let path = Bundle.main.path(forResource: "WEATHERinfo", ofType: "html") else { return }
-		let url = URL(fileURLWithPath:path)
-		let request = URLRequest(url: url)
-		
-		webView.load(request)
-		
-	}
-	
 
 }
+
